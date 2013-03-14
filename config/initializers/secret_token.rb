7b37dd3a66f370660e7f33b6337429098137183b
Mason Shin <mason.shin@spirent.com>
2013-03-13 18:51:04 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CanjsEx::Application.config.secret_token = '0fb3e3f4af380d24f0f690526c743d8352928c91884d1c7ae18f542e0bf6ccb3795a02fd7fa59a613c9a3ab1cc9944177cfccd3eac39e40783d575d067c4a3c6'
